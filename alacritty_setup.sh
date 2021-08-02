2192e9ff14c91d913fffba11822c80130d0af78c
remon2207 <reimond2207@gmail.com>
2021-08-02 19:23:20 +0900

    alacritty install

--- alacritty_setup.sh
+++ alacritty_setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # curlのインストール確認
-echo "curlがインストールされているか確認"
 which curl > /dev/null 2>&1
 if [ "$?" -eq 0 ] ; then
     echo "インストール済み"
@@ -14,37 +13,30 @@ else
 fi
 
 # ソースコードのダウンロード
-echo "alacrittyをダウンロード"
 git clone https://github.com/alacritty/alacritty.git ~/alacritty
 cd ~/alacritty
 
 # Rustのインストール
-echo "Rustのインストール"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 
 # 依存関係のインストール
-echo "依存関係のインストール"
 yes | sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 
 # ビルド
-echo "alacrittyのビルド"
 cargo build --release
 
 # ビルド後の設定
 # Terminfo
-echo "Terminfoの設定"
 sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
 infocmp alacritty
 
 # デスクトップエントリ
-echo "デスクトップエントリの作成"
 sudo cp target/release/alacritty /usr/local/bin
 sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 sudo desktop-file-install extra/linux/Alacritty.desktop
 sudo update-desktop-database
 
 # マニュアルページ
-echo "マニュアルページの設定"
 sudo mkdir -p /usr/local/share/man/man1
 gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
