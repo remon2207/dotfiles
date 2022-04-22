f144769d1663c2696a0c4e495107ad5c5c9cf3e8
remon2207 <reimond2207@gmail.com>
2022-04-22 15:15:10 +0900

    add fonts directory

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash -x
 
 THIS_DIR=$(cd $(dirname $0); pwd)
 
@@ -10,20 +10,10 @@ for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
+    [[ "$f" == ".config" ]] && continue
     [[ "$f" == ".gitignore" ]] && continue
 
     ln -snfv ~/dotfiles/"$f" $HOME/
 done
-# ln -snfv ~/dotfiles/.config/* $HOME/.config/
-
-# for d_config in alacritty conky bat i3 i3status-rust nvim picom ranger rofi
-# do
-#     [[ "$d_config" == ".git" ]] && continue
-#     [[ "$d_config" == ".DS_Store" ]] && continue
-
-#     ln -snfv ~/git/dotfiles/.config/"$d_config" $HOME/.config/
-# done
-
-# mkdir ~/.config/nvim
-# ln -snfv ~/git/dotfiles/.config/nvim/dein.toml ~/.config/nvim/
-# ln -snfv ~/git/dotfiles/.config/nvim/init.vim ~/.config/nvim/
+ln -snfv ~/dotfiles/.config/* $HOME/.config/
+sudo cp -rf fonts /usr/share/
