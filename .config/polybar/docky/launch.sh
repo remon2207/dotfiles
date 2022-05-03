15f4153a46a99f888d2d8db011b46febeaf42275
remon2207 <reimond2207@gmail.com>
2022-05-03 12:12:12 +0900

    update 2022/05/03

--- .config/polybar/docky/launch.sh
+++ .config/polybar/docky/launch.sh
@@ -11,4 +11,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+polybar DP-2 -c "$DIR"/config.ini &
+polybar HDMI-0 -c "$DIR"/config.ini &
+polybar DVI-D-0 -c "$DIR"/config.ini &
