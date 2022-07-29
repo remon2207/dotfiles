a957a37ad7fc5ba9d874b0280aefe5327d768789
remon2207 <reimond2207@gmail.com>
2022-07-29 09:32:16 +0900

    Update 2022/07/29 09:32:16

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
-THIS_DIR=$(cd $(dirname $0); pwd)
+THIS_DIR=$(cd $(dirname ${0}); pwd)
 
 echo "start setup..."
 
 for f in ${THIS_DIR}/.??*
 do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
-    [[ "$f" == ".config" ]] && continue
-    [[ "$f" == ".gitignore" ]] && continue
+    [[ ${f} == ".git" ]] && continue
+    [[ ${f} == ".DS_Store" ]] && continue
+    [[ ${f} == ".config" ]] && continue
+    [[ ${f} == ".gitignore" ]] && continue
 
-    ln -snfv ${f} $HOME/
+    ln -snfv ${f} ${HOME}/
 done
 
 for conf in ${THIS_DIR}/.config/*
 do
-  ln -snfv ${conf} $HOME/.config/
+  ln -snfv ${conf} ${HOME}/.config/
 done
 
-sudo ln -snfv ${THIS_DIR}/.gtkrc-2.0 /etc/gtk-2.0/gtkrc
-sudo ln -snfv ${THIS_DIR}/.config/gtk-3.0/settings.ini /etc/gtk-3.0/settings.ini
+# sudo ln -snfv ${THIS_DIR}/.gtkrc-2.0 /etc/gtk-2.0/gtkrc
+# sudo ln -snfv ${THIS_DIR}/.config/gtk-3.0/settings.ini /etc/gtk-3.0/settings.ini
 
 echo "end setup"
