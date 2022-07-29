0b4fa6fb1c61776f715b1e2c161bfe38e3b369ed
remon2207 <reimond2207@gmail.com>
2022-07-29 14:41:27 +0900

    Update 2022/07/29 14:41:27

--- arch_base_setup.sh
+++ arch_base_setup.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --noconfirm base-devel
-
+sudo sed -i "s/^#NTP=/NTP=0.asia.pool.ntp.org 1.asia.pool.ntp.org 2.asia.pool.ntp.org 3.asia.pool.ntp.org/" /etc/systemd/timesyncd.conf
+sudo sed -i "s/^#FallbackNTP/FallbackNTP/" /etc/systemd/timesyncd.conf
 sudo sed -i "s/-march=x86-64 -mtune=generic/-march=native/" /etc/makepkg.conf
 sudo sed -i 's/^#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(($(nproc)+1))"/' /etc/makepkg.conf
 sudo sed -i "s/^#BUILDDIR/BUILDDIR/" /etc/makepkg.conf
 sudo sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -z --threads=0 -)/" /etc/makepkg.conf
 sudo sed -i "^#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=10s/" /etc/systemd/system.conf
+
+sudo pacman -S --noconfirm base-devel
 
 cd $(dirname $0)
 ./arch_setup.sh
