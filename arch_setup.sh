d5ac4eb188a927a6a74e70584b41c52ff39d6d1f
remon2207 <reimond2207@gmail.com>
2022-07-29 12:36:56 +0900

    Update 2022/07/29 12:36:56

--- arch_setup.sh
+++ arch_setup.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
 
-rustup default stable
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
-paru -S --skipreview google-chrome ghq-bin postman-bin visual-studio-code-bin spotify ttf-cica
-mkdir ${HOME}/.cache/paru/clone/jdim-git
-cd $_
-paru -G jdim-git
-sed -i "s/^source=('git/source=('git+https" PKGBUILD
-makepkg -si
+paru_install() {
+    rustup default stable
+    git clone https://aur.archlinux.org/paru.git
+    cd paru
+    makepkg -si
+}
+
+aur_install() {
+    paru -S --skipreview google-chrome ghq-bin postman-bin visual-studio-code-bin spotify ttf-cica
+    mkdir ${HOME}/.cache/paru/clone/jdim-git
+    cd $_
+    paru -G jdim-git
+    sed -i "s/^source=('git/source=('git+https" PKGBUILD
+    makepkg -si
+}
+
+2chproxy() {
+    cd ${HOME}
+    ghq get yama-natuki/2chproxy.pl
+    mkdir ${HOME}/.local/bin/
+    ./ghq/github.com/yama-natuki/2chproxy.pl/install.sh ${HOME}/.local/bin
+}
+
+paru_install
+aur_install
+2chproxy
