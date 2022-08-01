a1ae4ec15b49a89759eb1f8c74744ca87d2f0838
remon2207 <39646984+remon2207@users.noreply.github.com>
2022-08-01 17:04:15 +0900

    Update cifs_setup.sh

--- cifs_setup.sh
+++ cifs_setup.sh
@@ -7,17 +7,18 @@ elif [ ${1} == "--help" ]; then
 Usage:
 ${0} <username> <password>
 EOF
-exit
-fi
+    exit
+else
+    pacman -Q cifs-utils > /dev/null 2>&1
+    if [ ${?} -eq 1 ]; then
+        sudo pacman -S --noconfirm cifs-utils
+    fi
 
-pacman -Q cifs-utils > /dev/null 2>&1
-if [ ${?} -eq 1 ]; then
-    sudo pacman -S --noconfirm cifs-utils
-fi
+    echo -e "username=${1}\n\
+    password=${2}" | sudo tee -a /etc/cifs-utils/.samba-rh
 
-echo -e "username=${1}\n\
-password=${2}" | sudo tee -a /etc/cifs-utils/.samba-rh
-
-sudo mkdir /mnt/RH
-echo -e "# /mnt/RH\n\
+    sudo mkdir /mnt/RH
+    echo -e "# /mnt/RH\n\
 //192.168.1.73/RH /mnt/RH cifs credentials=/etc/cifs-utils/.samba-rh,iocharset=utf8,uid=1000,gid=1000,file_mode=0644,dir_mode=0755,rw,nodev,nosuid,noexec,nofail,noauto,_netdev,x-systemd.automount,x-systemd.device-timeout=10,x-systemd.idle-timeout=1min 0 0" | sudo tee -a /etc/fstab
+    exit
+fi
