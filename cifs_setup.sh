7f6ec9077d7ee508100236a63edb5493444d1d9a
remon2207 <reimond2207@gmail.com>
2022-08-02 04:41:15 +0900

    Update 2022/08/02 04:41:14

--- cifs_setup.sh
+++ cifs_setup.sh
@@ -14,11 +14,16 @@ else
         sudo pacman -S --noconfirm cifs-utils
     fi
 
-    echo -e "username=${1}\n\
-password=${2}" | sudo tee -a /etc/cifs-utils/.samba-rh
+    cat << EOF | sudo tee /etc/cifs-utils/.samba-rh > /dev/null
+username=${1}
+password=${2}
+EOF
 
     sudo mkdir /mnt/RH
-    echo -e "# /mnt/RH\n\
-//192.168.1.73/RH /mnt/RH cifs credentials=/etc/cifs-utils/.samba-rh,iocharset=utf8,uid=1000,gid=1000,file_mode=0644,dir_mode=0755,rw,nodev,nosuid,noexec,nofail,noauto,_netdev,x-systemd.automount,x-systemd.device-timeout=10,x-systemd.idle-timeout=1min 0 0" | sudo tee -a /etc/fstab
+    cat << EOF | sudo tee -a /etc/fstab > /dev/null
+# /mntRH
+//192.168.1.73/RH /mnt/RH cifs credentials=/etc/cifs-utils/.samba-rh,iocharset=utf8,uid=1000,gid=1000,file_mode=0644,dir_mode=0755,rw,nodev,nosuid,noexec,nofail,noauto,_netdev,x-systemd.automount,x-systemd.device-timeout=10,x-systemd.idle-timeout=1min 0 0
+EOF
+
     exit 0
 fi
