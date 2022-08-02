8519bb6a1c15acdda38e54e02e150a101c383f08
remon2207 <39646984+remon2207@users.noreply.github.com>
2022-08-02 13:56:43 +0900

    Update cifs_setup.sh

--- cifs_setup.sh
+++ cifs_setup.sh
@@ -24,6 +24,7 @@ EOF
 # /mntRH
 //192.168.1.73/RH /mnt/RH cifs vers=3.0,credentials=/etc/cifs-utils/.samba-rh,iocharset=utf8,uid=1000,gid=1000,file_mode=0644,dir_mode=0755,rw,nodev,nosuid,noexec,nofail,noauto,_netdev,x-systemd.automount,x-systemd.requires=network-online.target,x-systemd.device-timeout=10,x-systemd.idle-timeout=1min 0 0
 EOF
+    sudo systemctl daemon-reload
 
     exit 0
 fi
