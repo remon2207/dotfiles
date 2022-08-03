db2207fc35adc6d572d3d3be6bd871862afc7dd8
remon2207 <39646984+remon2207@users.noreply.github.com>
2022-08-04 04:16:46 +0900

    Update arch_setup.sh

--- arch_setup.sh
+++ arch_setup.sh
@@ -12,7 +12,7 @@ paru_install() {
 }
 
 aur_install() {
-    paru -S --noconfirm --needed google-chrome ghq-bin postman-bin visual-studio-code-bin spotify ttf-cica downgrade virtualbox-ext-oracle
+    paru -S --noconfirm --needed gamin google-chrome ghq-bin postman-bin visual-studio-code-bin spotify ttf-cica downgrade virtualbox-ext-oracle
     mkdir -p ${HOME}/.cache/paru/clone/
     cd $_
     paru -G jdim-git
@@ -58,12 +58,12 @@ EOF
     return 0
 }
 
-get_de=$(neofetch | grep "DE" | awk -F ':' '{print $2}' | awk -F ' ' '{print $2}')
-if [ ${get_de} == "GNOME" ]; then
-    gnome
-elif [ ${get_de} == "Xfce" ]; then
-    paru -S --noconfirm --needed gamin
-fi
+# get_de=$(neofetch | grep "DE" | awk -F ':' '{print $2}' | awk -F ' ' '{print $2}')
+# if [ ${get_de} == "GNOME" ]; then
+#     gnome
+# elif [ ${get_de} == "Xfce" ]; then
+#     paru -S --noconfirm --needed gamin
+# fi
 paru_install
 aur_install
 2chproxy
