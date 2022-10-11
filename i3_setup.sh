9c873391a0971ba26e23d70147a57604b332f03c
remon2207 <39646984+remon2207@users.noreply.github.com>
2022-10-12 08:40:04 +0900

    Update i3_setup.sh

--- i3_setup.sh
+++ i3_setup.sh
@@ -10,12 +10,18 @@ home_symbolic=(".gtkrc-2.0" ".tmux.conf" ".xinitrc" ".Xresources" ".zprofile" ".
 conf_symbolic=("alacritty" "bat" "dunst" "fontconfig" "gtk-3.0" "htop" "i3" "neofetch" "nvim" "picom" "polybar" "ranger" "rofi" "silicon" "starship.toml")
 
 repeat() {
+    rm -rf ${HOME}/.xinitrc ${HOME}/.config/i3
+
     for home in ${home_symbolic[@]}; do
-        echo "${current_dir}/${home}"
+        ln -snfv ${current_dir}/${home} ${HOME}/
     done
     for conf in ${conf_symbolic[@]}; do
-        echo "${current_dir}/.config/${conf}"
+        ln -snfv ${current_dir}/.config/${conf} ${HOME}/.config/
     done
+
+    sudo mkdir /etc/gtk-2.0
+    sudo ln -s ${HOME}/.gtkrc-2.0 /etc/gtk-2.0/gtkrc
+    sudo ln -s ${HOME}/.config/gtk-3.0/settings.ini /etc/gtk-3.0
 }
 
 repeat
