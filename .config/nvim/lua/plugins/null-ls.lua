e9676facd54d1b2127e388a05b10d78113ec02f6
remon2207 <39646984+remon2207@users.noreply.github.com>
2022-10-13 18:59:09 +0900

    Update null-ls.lua

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -38,14 +38,14 @@ null_ls.setup({
     },
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    lsp_formatting(bufnr)
-                end,
-            })
+            -- vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            -- vim.api.nvim_create_autocmd("BufWritePre", {
+            --     group = augroup,
+            --     buffer = bufnr,
+            --     callback = function()
+            --         lsp_formatting(bufnr)
+            --     end,
+            -- })
             vim.keymap.set("n", "<Leader>fa", function()
                 lsp_formatting(bufnr)
             end, { noremap = true, silent = true })
