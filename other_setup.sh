56ba44567b79d46ad63dabdf0ca4d332e72eb6a7
remon2207 <reimond2207@gmail.com>
2022-10-16 17:47:14 +0900

    Change filename and Formatting with shfmt

--- other_setup.sh
+++ other_setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# npm setup
 source /usr/share/nvm/init-nvm.sh
 nvm install --lts
 npm i -g eslint_d @fsouza/prettierd @johnnymorganz/stylua-bin
+
+# go setup
+go install mvdan.cc/sh/v3/cmd/shfmt@latest
