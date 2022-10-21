f247f7b540e7ff5461e2bb0e026314971da2af99
remon2207 <reimond2207@gmail.com>
2022-10-21 20:37:22 +0900

    Change indent color

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -3,13 +3,14 @@ if not status then
     return
 end
 
-indent.setup({
-    char_highlight_list = {
-        'IndentBlanklineIndent1',
-    },
-})
-
-vim.cmd([[highlight IndentBlanklineIndent1 guifg=#586e75 gui=nocombine]])
+-- vim.cmd([[highlight IndentBlanklineIndent guifg=#586e75 gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineIndent guifg=#094655 gui=nocombine]])
 
 vim.g.indent_blankline_char_blankline = ''
 vim.g.indent_blankline_use_treesitter = true
+
+indent.setup({
+    char_highlight_list = {
+        'IndentBlanklineIndent',
+    },
+})
