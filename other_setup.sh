9d75a054982c9bfd667ec700c399d14adb7d2c60
remon2207 <reimond2207@gmail.com>
2022-10-21 09:30:23 +0900

    Install package

--- other_setup.sh
+++ other_setup.sh
@@ -3,7 +3,7 @@
 # npm setup
 source /usr/share/nvm/init-nvm.sh
 nvm install --lts
-npm i -g eslint_d @fsouza/prettierd @johnnymorganz/stylua-bin
+npm i -g eslint_d @fsouza/prettierd @johnnymorganz/stylua-bin cspell
 
 # go setup
 go install mvdan.cc/sh/v3/cmd/shfmt@latest
