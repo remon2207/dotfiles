d58cc11a1aa3c1a20dd4da7aa4d819f89b793feb
remon2207 <reimond2207@gmail.com>
2022-10-24 20:56:18 +0900

    update: 2022/10/24 20:56:18

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -49,105 +49,7 @@ return {
             },
         },
     },
-    -- iceberg
-    -- colors = {
-    --   tab_bar = {
-    --     background = "#1e2132",
-    --     active_tab = {
-    --       bg_color = "#3d425b",
-    --       fg_color = "#c6c8d1",
-    --       intensity = "Normal",
-    --       underline = "None",
-    --       italic = false,
-    --       strikethrough = false,
-    --     },
-    --     inactive_tab = {
-    --       bg_color = "#242940",
-    --       fg_color = "#808080",
-    --     },
-    --     inactive_tab_hover = {
-    --       bg_color = "#3e445e",
-    --       fg_color = "#c6c8d1",
-    --       italic = false,
-    --     },
-    --     new_tab = {
-    --       bg_color = "#515e97",
-    --       fg_color = "#c6c8d1",
-    --     },
-    --     new_tab_hover = {
-    --       bg_color = "#515e97",
-    --       fg_color = "#c6c8d1",
-    --       italic = false,
-    --     }
-    --   },
-    -- },
-    -- Nord
-    -- colors = {
-    --   tab_bar = {
-    --     background = "#2E3440",
-    --     active_tab = {
-    --       bg_color = "#4C566A",
-    --       fg_color = "#ECEFF4",
-    --       intensity = "Normal",
-    --       underline = "None",
-    --       italic = false,
-    --       strikethrough = false,
-    --     },
-    --     inactive_tab = {
-    --       bg_color = "#3B4252",
-    --       fg_color = "#D8DEE9",
-    --     },
-    --     inactive_tab_hover = {
-    --       bg_color = "#434C5E",
-    --       fg_color = "#E5E9F0",
-    --       italic = false,
-    --     },
-    --     new_tab = {
-    --       bg_color = "#434C5E",
-    --       fg_color = "#D8DEE9",
-    --     },
-    --     new_tab_hover = {
-    --       bg_color = "#4C566A",
-    --       fg_color = "#E5E9F0",
-    --       italic = false,
-    --     }
-    --   },
-    -- },
-    -- Dracula
-    -- colors = {
-    --   tab_bar = {
-    --     background = "#282a36",
-    --     active_tab = {
-    --       bg_color = "#44475a",
-    --       fg_color = "#f8f8f2",
-    --       intensity = "Normal",
-    --       underline = "None",
-    --       italic = false,
-    --       strikethrough = false,
-    --     },
-    --     inactive_tab = {
-    --       bg_color = "#242940",
-    --       fg_color = "#6272a4",
-    --     },
-    --     inactive_tab_hover = {
-    --       bg_color = "#3e445e",
-    --       fg_color = "#f8f8f2",
-    --       italic = false,
-    --     },
-    --     new_tab = {
-    --       bg_color = "#44475a",
-    --       fg_color = "#f8f8f2",
-    --     },
-    --     new_tab_hover = {
-    --       bg_color = "#6272a4",
-    --       fg_color = "#f8f8f2",
-    --       italic = false,
-    --     }
-    --   },
-    -- },
     keys = {
-        -- {key = "[", mods = "CTRL", action=wezterm.action{ActivateTabRelative=-1}},
-        -- {key = "]", mods = "CTRL", action=wezterm.action{ActivateTabRelative=1}},
         { key = 'p', mods = 'CTRL|SHIFT', action = wezterm.action({ ScrollByLine = -1 }) },
         { key = 'n', mods = 'CTRL|SHIFT', action = wezterm.action({ ScrollByLine = 1 }) },
         { key = 'b', mods = 'CTRL|SHIFT', action = wezterm.action({ ScrollByPage = -1 }) },
@@ -156,7 +58,5 @@ return {
         { key = 'PageUp', mods = 'CTRL|SHIFT', action = wezterm.action({ MoveTabRelative = -1 }) },
         { key = 'PageDown', mods = 'CTRL|SHIFT', action = wezterm.action({ MoveTabRelative = 1 }) },
     },
-    -- disable_default_key_bindings = true,
-    -- exit_behavior = 'Close',
-    warn_about_missing_glyphs = false,
+    -- warn_about_missing_glyphs = false,
 }
