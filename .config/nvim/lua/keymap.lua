60d7f62cafdbc5dcae7263372d3736aa9ecd8273
remon2207 <reimond2207@gmail.com>
2022-11-06 17:38:33 +0900

    Add keymap

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -21,6 +21,10 @@ keymap.set('n', 'L', '<Cmd>bnext<CR>', opts)
 
 keymap.set('n', 'x', '"_x', opts)
 keymap.set('n', 'cit', '"_cit', opts)
+keymap.set('n', "ci'", '"_ci\'', opts)
+keymap.set('n', 'ci"', '"_ci"', opts)
+keymap.set('n', 'ci{', '"_ci{', opts)
+keymap.set('n', 'ci}', '"_ci}', opts)
 
 keymap.set('n', '<ScrollWheelUp>', '<Nop>', opts)
 keymap.set('n', '<ScrollWheelDown>', '<Nop>', opts)
