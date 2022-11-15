9ec30c38b5bada40a8bc0e8fab1f1628c7357c8d
remon2207 <reimond2207@gmail.com>
2022-11-15 09:55:24 +0900

    update: 2022/11/15 09:55:15

--- .config/zsh/config.d/zinit.zsh
+++ .config/zsh/config.d/zinit.zsh
@@ -1,4 +1,4 @@
-if [[ -n "$DISPLAY" ]]; then
+zinit_install() {
   if [[ ! -f "$HOME/.local/share/zinit/zinit.git/zinit.zsh" ]]; then
     print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
     command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
@@ -10,6 +10,10 @@ if [[ -n "$DISPLAY" ]]; then
   source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
   autoload -Uz _zinit
   (( ${+_comps} )) && _comps[zinit]=_zinit
+}
+
+if [[ -n "$DISPLAY" ]]; then
+  zinit_install
 
   zinit light Aloxaf/fzf-tab
   zinit light zsh-users/zsh-syntax-highlighting
@@ -21,17 +25,13 @@ if [[ -n "$DISPLAY" ]]; then
   ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=10"
 
 else
-  ### Added by Zinit's installer
-  if [[ ! -f "$HOME/.local/share/zinit/zinit.git/zinit.zsh" ]]; then
-    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
-    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
-    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
-      print -P "%F{33} %F{34}Installation successful.%f%b" || \
-      print -P "%F{160} The clone has failed.%f%b"
-  fi
+  zinit_install
 
-  source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
-  autoload -Uz _zinit
-  (( ${+_comps} )) && _comps[zinit]=_zinit
-  ### End of Zinit's installer chunk
+  zinit light Aloxaf/fzf-tab
+  zinit light zsh-users/zsh-syntax-highlighting
+  zinit light zsh-users/zsh-autosuggestions
+  zinit light zsh-users/zsh-completions
+  zinit light rupa/z
 fi
+
+unfunction zinit_install
