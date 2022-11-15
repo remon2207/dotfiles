37369ecdddb897a8e59b51b91c9f191b6cd5900d
remon2207 <reimond2207@gmail.com>
2022-11-15 09:42:06 +0900

    Add symbolic link

--- i3_setup.sh
+++ i3_setup.sh
@@ -76,6 +76,7 @@ setup() {
 
   sudo ln -s $HOME/.gtkrc-2.0 /etc/gtk-2.0/gtkrc
   sudo ln -s $HOME/.config/gtk-3.0/settings.ini /etc/gtk-3.0
+  sudo ln -snfv $current_dir/usr/local/bin/display_nouveau_setup.sh /usr/local/bin
 
   ln -s $current_dir/.local/share/applications/lazydocker.desktop $HOME/.local/share/applications/
   ln -s $current_dir/.local/share/applications/bghtop.desktop $HOME/.local/share/applications/
