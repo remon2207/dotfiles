f6abe9de683133911ae1ec5a0c2c8e1c74f2744d
remon2207 <reimond2207@gmail.com>
2022-11-15 15:08:46 +0900

    Formatting with stylua

--- .config/nvim/lua/plugins/nord.lua
+++ .config/nvim/lua/plugins/nord.lua
@@ -1,7 +1,3 @@
-
-
 vim.g.nord_italic = false
 vim.g.nord_borders = true
 vim.g.nord_contrast = false
-require('nord').set
-
--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -74,20 +74,20 @@ local on_attach = function(client, bufnr)
   })
 
   vim.lsp.handlers['textDocument/hover'] = function(_, result, ctx, config)
-      -- config = config or {}
-      config = popup_opts
-      config.focus_id = ctx.method
-      if not (result and result.contents) then
-          -- vim.notify('No information available')
-          return
-      end
-      local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
-      markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
-      if vim.tbl_isempty(markdown_lines) then
-          -- vim.notify('No information available')
-          return
-      end
-      return vim.lsp.util.open_floating_preview(markdown_lines, 'markdown', config)
+    -- config = config or {}
+    config = popup_opts
+    config.focus_id = ctx.method
+    if not (result and result.contents) then
+      -- vim.notify('No information available')
+      return
+    end
+    local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+    markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
+    if vim.tbl_isempty(markdown_lines) then
+      -- vim.notify('No information available')
+      return
+    end
+    return vim.lsp.util.open_floating_preview(markdown_lines, 'markdown', config)
   end
 
   vim.diagnostic.config({
