997d043be13802331c2cd99b9897dd5bb28e6971
remon2207 <reimond2207@gmail.com>
2022-11-15 09:29:48 +0900

    update: 2022/11/15 09:06:39

--- cifs_setup.sh
+++ cifs_setup.sh
@@ -19,10 +19,13 @@ username="$1"
 password="$2"
 EOF
 
-  sudo mkdir /mnt/RH
+  sudo mkdir /mnt/{RH,linux-windows}
   cat << EOF | sudo tee -a /etc/fstab > /dev/null
 # /mnt/RH
 //192.168.1.80/RH /mnt/RH cifs vers=3.0,credentials=/etc/cifs-utils/.samba,iocharset=utf8,uid=1000,gid=1000,file_mode=0644,dir_mode=0755,rw,nodev,nosuid,noatime,noexec,nofail,noauto,_netdev,x-systemd.automount,x-systemd.requires=network-online.target,x-systemd.device-timeout=10,x-systemd.idle-timeout=1min 0 0
+
+# /mnt/linux-windows
+PARTUUID=fe32ec92-a0c4-4aa8-ad27-a562129f1f04 /mnt/linux-windows ntfs-3g uid=1000,gid=1000,dmask=022,fmask=133,nodev,nosuid,noatime,noexec,nofail,noauto 0 0
 EOF
   sudo systemctl daemon-reload
 
