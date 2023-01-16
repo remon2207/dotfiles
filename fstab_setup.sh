9a9f17269ce50a206bd2b07411fc0a7d7eb03e00
remon2207 <39646984+remon2207@users.noreply.github.com>
2023-01-16 12:16:22 +0900

    Update fstab_setup.sh

--- fstab_setup.sh
+++ fstab_setup.sh
@@ -22,8 +22,8 @@ EOF
   sudo mkdir /mnt/{share01,linux-windows}
   sudo mkdir /mnt/share01/{rh,server}
   cat << EOF | sudo tee -a /etc/fstab > /dev/null
-# /mnt/RH
-//192.168.1.33/rh /mnt/share01/rh cifs vers=3.0,credentials=/etc/cifs-utils/.samba,iocharset=utf8,uid=1000,gid=1000,file_mode=0644,dir_mode=0755,rw,nodev,nosuid,noatime,noexec,nofail,noauto,nomand,noiversion,norelatime,lazytime,_netdev,x-systemd.automount,x-systemd.idle-timeout=1min 0 0
+# /mnt/share01/rh
+#//192.168.1.33/rh /mnt/share01/rh cifs vers=3.0,credentials=/etc/cifs-utils/.samba,iocharset=utf8,uid=1000,gid=1000,file_mode=0644,dir_mode=0755,rw,nodev,nosuid,noatime,noexec,nofail,noauto,nomand,noiversion,norelatime,lazytime,_netdev,x-systemd.automount,x-systemd.idle-timeout=1min 0 0
 
 # /mnt/linux-windows
 PARTUUID=fe32ec92-a0c4-4aa8-ad27-a562129f1f04 /mnt/linux-windows ntfs-3g uid=1000,gid=1000,dmask=022,fmask=133,nodev,nosuid,noatime,noexec,nofail,noauto,nomand,noiversion,norelatime,lazytime,x-systemd.automount,x-systemd.idle-timeout=1min 0 0
