dc0b28fee33f2f8871be0865edaac2ae66bfda04
remon2207.develop@gmail.com <remon2207>
2023-10-08 14:06:03 +0900

    update: 2023/10/08 13:50:21

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,16 +1,17 @@
-local status, indent = pcall(require, 'indent_blankline')
+-- local status, indent = pcall(require, 'indent_blankline')
+local status, ibl = pcall(require, 'ibl')
 if not status then
   return
 end
 
 -- vim.cmd([[highlight IndentBlanklineIndent guifg=#586e75 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent guifg=#094655 gui=nocombine]])
+-- vim.cmd([[highlight IndentBlanklineIndent guifg=#094655 gui=nocombine]])
 
-vim.g.indent_blankline_char_blankline = ''
-vim.g.indent_blankline_use_treesitter = true
+-- vim.g.indent_blankline_char_blankline = ''
+-- vim.g.indent_blankline_use_treesitter = true
 
-indent.setup({
-  char_highlight_list = {
-    'IndentBlanklineIndent',
-  },
+ibl.setup({
+  -- char_highlight_list = {
+  --   'IndentBlanklineIndent',
+  -- },
 })
