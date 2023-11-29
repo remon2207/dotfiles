afcb2cdace23bf53544eb596f38a8b757b8e8a82
remon2207 <remon2207.develop@gmail.com>
2023-11-30 00:46:35 +0900

    2023/11/30 00:45:08

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,5 +1,5 @@
 psidkill() {
-  ps_id="$(ps aux | rg -iN "${1}" | awk 'NR==1 {print $2}')"
+  ps_id="$(ps aux | rg --ignore-case --no-line-number "${1}" | awk 'NR==1 {print $2}')"
 
   kill ${ps_id}
 
@@ -26,9 +26,9 @@ EOF
   alacritty_conf="${dotfiles}/.config/alacritty/conf.d/env.yml"
 
   replacements() {
-    sd -s "${1}" "${3}" "${mimeapps}"
-    sd -s "${2}" "${4}" "${kitty_conf}"
-    sd -s "${2}" "${4}" "${alacritty_conf}"
+    sd --string-mode "${1}" "${3}" "${mimeapps}"
+    sd --string-mode "${2}" "${4}" "${kitty_conf}"
+    sd --string-mode "${2}" "${4}" "${alacritty_conf}"
   }
 
   while getopts 'c:n:h' opt; do
@@ -80,18 +80,18 @@ EOF
   i3_conf="${dotfiles}/.config/i3/conf.d/appstart_keybind.conf"
   bghtop="${dotfiles}/.local/share/applications/bghtop.desktop"
   alacritty='bindsym $mod+Return exec --no-startup-id alacritty'
-  alacritty_ranger='bindsym $mod+e exec --no-startup-id alacritty -e ranger'
+  alacritty_ranger='bindsym $mod+e exec --no-startup-id alacritty --command ranger'
   kitty='bindsym $mod+Return exec --no-startup-id kitty'
-  kitty_ranger='bindsym $mod+e exec --no-startup-id kitty -1 ranger'
+  kitty_ranger='bindsym $mod+e exec --no-startup-id kitty --single-instance ranger'
   wezterm='bindsym $mod+Return exec --no-startup-id wezterm'
-  wezterm_ranger='bindsym $mod+e exec --no-startup-id wezterm -e ranger'
+  wezterm_ranger='bindsym $mod+e exec --no-startup-id wezterm start ranger'
 
   replacements() {
-    sd -s "${1}" "# ${1}" "${i3_conf}"
-    sd -s "${2}" "# ${2}" "${i3_conf}"
+    sd --string-mode "${1}" "# ${1}" "${i3_conf}"
+    sd --string-mode "${2}" "# ${2}" "${i3_conf}"
 
-    sd -s "# ${3}" "${3}" "${i3_conf}"
-    sd -s "# ${4}" "${4}" "${i3_conf}"
+    sd --string-mode "# ${3}" "${3}" "${i3_conf}"
+    sd --string-mode "# ${4}" "${4}" "${i3_conf}"
 
     sd "^Exec=${5}" "# Exec=${5}" "${bghtop}"
     sd "^# Exec=${6}" "Exec=${6}" "${bghtop}"
@@ -154,13 +154,13 @@ bootusb() {
 tofish() {
   dotfiles="${HOME}/ghq/github.com/remon2207/dotfiles"
 
-  startline="$(("$(cat "${HOME}/.zshrc" | rg -n 'load fish' | cut -d ':' -f 1)" + 1))"
+  startline="$(("$(bat --plain "${HOME}/.zshrc" | rg --line-number 'load fish' | cut --delimiter=':' --fields=1)" + 1))"
 
-  endline="$(cat "${HOME}/.zshrc" | wc -l)"
+  endline="$(bat --plain "${HOME}/.zshrc" | wc --lines)"
 
-  sed -i -e "${startline},${endline}s/^# //" "${dotfiles}/.zshrc"
-  sed -i -e '2s/^/# /' "${dotfiles}/.tmux.conf"
-  sed -i -e '3s/^# //' "${dotfiles}/.tmux.conf"
+  sed --in-place -expression="${startline},${endline}s/^# //" "${dotfiles}/.zshrc"
+  sed --in-place -expression='2s/^/# /' "${dotfiles}/.tmux.conf"
+  sed --in-place -expression='3s/^# //' "${dotfiles}/.tmux.conf"
 
   unset dotfiles startline endline
 
@@ -168,9 +168,9 @@ tofish() {
 }
 
 authycheck() {
-  result="$(curl -sSLH 'Snap-Device-Series: 16' https://api.snapcraft.io/v2/snaps/info/authy | jq)"
-  revision="$(echo "${result}" | rg 'revision' | awk -F '[ ":,]*' '{print $3}')"
-  version="$(echo "${result}" | rg 'version' | awk -F '[ ":]*' '{print $3}')"
+  result="$(curl -sSLH --silent --show-error --location --header 'Snap-Device-Series: 16' https://api.snapcraft.io/v2/snaps/info/authy | jq)"
+  revision="$(echo "${result}" | rg 'revision' | awk --field-separator='[ ":,]*' '{print $3}')"
+  version="$(echo "${result}" | rg 'version' | awk --field-separator='[ ":]*' '{print $3}')"
 
   echo "revision: ${revision}"
   echo "version: ${version}"
@@ -179,14 +179,14 @@ authycheck() {
 }
 
 pkgupgrade() {
-  distribution_name="$(rg '^PRETTY' /etc/os-release | awk -F '"' '{print $2}')"
+  distribution_name="$(rg '^PRETTY' /etc/os-release | awk --field-separator='"' '{print $2}')"
 
   case "${distribution_name}" in
   'Gentoo Linux')
     sudo emerge-webrsync
-    sudo emaint sync -a
-    sudo emerge -avuDN @world
-    sudo emerge -a --depclean
+    sudo emaint sync --auto
+    sudo emerge --ask --verbose --update --deep --newuse @world
+    sudo emerge --ask --depclean
     ;;
   'Arch Linux')
     checkupdates
@@ -244,11 +244,11 @@ EOF
 }
 
 raspi-backup() { sudo dd if=/dev/sde conv=sync,noerror iflag=nocache oflag=nocache,dsync | pv | pigz > "${1}"; }
-mkcd() { mkdir -p "${1}" && cd "${_}"; }
-psgrep() { ps aux | rg -iN "${1}"; }
+mkcd() { mkdir --parents "${1}" && cd "${_}"; }
+psgrep() { ps aux | rg --ignore-case --no-line-number "${1}"; }
 shtouch() { touch "${1}.sh" && chmod +x "${_}" && nvim "${_}"; }
 nfind() { find "${@}" -not -path './.cache/*'; }
 sfind() { sudo find "${@}" -not \( -path "${HOME}/.cache/*" -o -path '/mnt/*' \); }
-chpwd() { [[ "$(pwd)" != "${OLDPWD}" ]] && lsd -AFI '.git'; }
-shellstart() { lsd -AFI '.git'; }
+chpwd() { [[ "$(pwd)" != "${OLDPWD}" ]] && lsd --almost-all --classify --ignore-glob '.git'; }
+shellstart() { lsd --almost-all --classify --ignore-glob '.git'; }
 shellstart
