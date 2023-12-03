48b4b605aa201befa3f6a9bc8b204b01a4fc7748
remon2207 <remon2207.develop@gmail.com>
2023-12-03 12:57:43 +0900

    2023/12/03 12:41:04

--- bin/load_layout.sh
+++ bin/load_layout.sh
@@ -7,7 +7,8 @@ i3-msg "workspace 3; append_layout ${HOME}/.config/i3/workspace_3.json"
 i3-msg "workspace 1; append_layout ${HOME}/.config/i3/workspace_1.json"
 
 # And finally we fill the containers with the programs they had
-(discord &)
-(slack &)
 (kitty --single-instance htop &)
+(kitty --single-instance nvtop &)
+(slack &)
 (vivaldi-stable &)
+(discord &)
