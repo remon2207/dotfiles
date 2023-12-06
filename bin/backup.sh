f9557bd39204e54869e578f29563c02fb2797384
remon2207 <remon2207.develop@gmail.com>
2023-12-07 02:42:17 +0900

    2023/12/07 02:42:17

--- bin/backup.sh
+++ bin/backup.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-rsync --archive --update --delete "${HOME}/Pictures/"* /mnt/share01/rh/pictures
-rsync --archive --update --delete "${HOME}/Videos/"* /mnt/share01/rh/videos
-rsync --archive --update --delete "${HOME}/.ssh/"* /mnt/share01/rh/ssh
+rsync --archive --update --delete "${HOME}/Pictures/" /mnt/share01/rh/pictures
+rsync --archive --update --delete "${HOME}/Videos/" /mnt/share01/rh/videos
+rsync --archive --update --delete "${HOME}/.ssh/" /mnt/share01/rh/ssh
