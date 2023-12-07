32d499e197588b3edd7b34da8328e59bbdefda76
remon2207 <remon2207.develop@gmail.com>
2023-12-08 06:18:58 +0900

    2023/12/08 06:18:58

--- i3_setup_arch.sh
+++ i3_setup_arch.sh
@@ -3,19 +3,19 @@
 set -eu
 
 current_dir=$(cd "$(dirname "${0}")" && pwd)
-desktop_entry_dir="${current_dir}/.local/share/applications"
 home_symbolic=(
   '.curlrc'
   '.gitconfig'
   '.gtkrc-2.0_arch'
   '.profile'
+  '.ripgreprc'
   '.tmux_arch.conf'
   '.wgetrc'
   '.xinitrc_arch'
   '.Xresources_arch'
   '.zshrc'
-  'commit.template'
   'bin'
+  'commit.template'
 )
 conf_symbolic=(
   'alacritty'
@@ -33,6 +33,7 @@ conf_symbolic=(
   'mimeapps.list'
   'neofetch'
   'nvim'
+  'nvtop'
   'picom'
   'polybar_arch'
   'procs'
@@ -43,17 +44,27 @@ conf_symbolic=(
   'wezterm'
   'zsh'
 )
+entry_symbolic=(
+  'mozc_config_arch.desktop'
+  'mozc_dictionary_arch.desktop'
+)
 
 setup() {
-  rm --recursive --force "${HOME}/.xinitrc" "${HOME}/.config/i3"
   mkdir --parents "${HOME}/.local/share/applications"
   sudo mkdir /etc/gtk-2.0
 
-  for home in "${home_symbolic[@]}"; do ln --symbolic --force --verbose "${current_dir}/${home}" "${HOME}"; done
-  for conf in "${conf_symbolic[@]}"; do ln --symbolic --force --verbose "${current_dir}/.config/${conf}" "${HOME}/.config"; done
-
-  ln --symbolic --force --verbose "${desktop_entry_dir}/mozc_config_arch.desktop" "${HOME}/.local/share/applications/mozc_config.desktop"
-  ln --symbolic --force --verbose "${desktop_entry_dir}/mozc_dictionary_arch.desktop" "${HOME}/.local/share/applications/mozc_dictionary.desktop"
+  for home in "${home_symbolic[@]}"; do
+    home_replaced="$(echo "${home}" | sed --expression='s/^\(.*\)_arch\(.*\)$/\1\2/')"
+    ln --symbolic --force --verbose "${current_dir}/${home}" "${HOME}/${home_replaced}"
+  done
+  for conf in "${conf_symbolic[@]}"; do
+    conf_replaced="$(echo "${conf}" | sed --expression='s/^\(.*\)_arch\(.*\)$/\1\2/')"
+    ln --symbolic --force --verbose "${current_dir}/.config/${conf}" "${HOME}/${conf_replaced}"
+  done
+  for entry in "${entry_symbolic[@]}"; do
+    entry_replaced="$(echo "${entry}" | sed --expression='s/^\(.*\)_arch\(.*\)$/\1\2/')"
+    ln --symbolic --force --verbose "${current_dir}/.local/share/applications/${entry}" "${HOME}/.local/share/applications/${entry_replaced}"
+  done
 
   sudo ln --symbolic --force --verbose "${HOME}/.gtkrc-2.0" /etc/gtk-2.0/gtkrc
   sudo ln --symbolic --force --verbose "${HOME}/.config/gtk-3.0/settings.ini" /etc/gtk-3.0
@@ -61,7 +72,7 @@ setup() {
 
 services() {
   cp --archive "${current_dir}/.config/systemd/user/ssh-agent.service" "${HOME}/.config/systemd/user"
-  cp --archive "${current_dir}/.config/systemd/user/auto-backup.{service,timer}" "${HOME}/.config/systemd/user"
+  cp --archive "${current_dir}/.config/systemd/user/"auto-backup.{service,timer} "${HOME}/.config/systemd/user"
   sudo cp --archive "${current_dir}/etc/systemd/system/auto-lock@.service" /etc/systemd/system
 
   systemctl --user enable --now ssh-agent.service
