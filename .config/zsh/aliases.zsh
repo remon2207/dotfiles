c1893858acf530e7aa84ef8ffa4962d19c6745c1
remon2207 <remon2207.develop@gmail.com>
2023-12-23 13:58:43 +0900

    2023/12/23 13:58:43

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -100,6 +100,7 @@ alias procstree='procs --tree'
 alias rg='rg --threads="$(("$(nproc)"+1))"'
 alias versioncheck="bat --plain /etc/os-release | awk --field-separator='\"' 'END {print}'; uname --kernel-release | sd '^([0-9].*)-.*$' 'KERNEL_VERSION=\"\$1\"'"
 alias sbat='sudo bat --theme="Solarized (dark)"'
+alias delta='delta --pager="/usr/bin/bat --plain"'
 
 case "${DISTRIBUTION_NAME}" in
 'archlinux')
