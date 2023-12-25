2320821bb1136fba5b4a85d20237540831eac838
remon2207 <remon2207.develop@gmail.com>
2023-12-25 16:13:31 +0900

    2023/12/25 16:13:31

--- bin/load_layout.sh
+++ bin/load_layout.sh
@@ -9,11 +9,11 @@ if [[ "${1}" == 'dvi' ]]; then
   # 展開に必要なプログラムを起動する
   (/usr/bin/discord &)
   (/usr/bin/slack &)
-  (/usr/bin/kitty --single-instance htop &)
-  # (/usr/bin/kitty --single-instance btop &)
-  (/usr/bin/kitty --single-instance nvtop &)
-  # (/usr/bin/wezterm start htop &)
-  # (/usr/bin/wezterm start nvtop &)
+  (/usr/bin/kitty --single-instance /usr/bin/htop &)
+  # (/usr/bin/kitty --single-instance /usr/bin/btop &)
+  (/usr/bin/kitty --single-instance /usr/bin/nvtop &)
+  # (/usr/bin/wezterm start /usr/bin/htop &)
+  # (/usr/bin/wezterm start /usr/bin/nvtop &)
 elif [[ "${1}" == 'dp' ]] || [[ "${1}" == 'dvi' ]] && [[ "${2}" == 'dp' ]] || [[ "${2}" == 'dvi' ]]; then
   # ワークスペース3に移動し、レイアウトを展開する
   for num in 3 1; do /usr/bin/i3-msg "workspace --no-auto-back-and-forth ${num}; append_layout ${XDG_CONFIG_HOME}/i3/workspace_${num}.json"; done
@@ -21,10 +21,10 @@ elif [[ "${1}" == 'dp' ]] || [[ "${1}" == 'dvi' ]] && [[ "${2}" == 'dp' ]] || [[
   # 展開に必要なプログラムを起動する
   (/usr/bin/discord &)
   (/usr/bin/slack &)
-  (/usr/bin/kitty --single-instance htop &)
-  # (/usr/bin/kitty --single-instance btop &)
-  (/usr/bin/kitty --single-instance nvtop &)
-  # (/usr/bin/wezterm start htop &)
-  # (/usr/bin/wezterm start nvtop &)
+  (/usr/bin/kitty --single-instance /usr/bin/htop &)
+  # (/usr/bin/kitty --single-instance /usr/bin/btop &)
+  (/usr/bin/kitty --single-instance /usr/bin/nvtop &)
+  # (/usr/bin/wezterm start /usr/bin/htop &)
+  # (/usr/bin/wezterm start /usr/bin/nvtop &)
   (/usr/bin/vivaldi-stable &)
 fi
