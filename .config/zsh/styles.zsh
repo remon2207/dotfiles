d15e1b031c894ea100f77fbafee34ac7fc318683
remon2207 <remon2207.develop@gmail.com>
2023-12-25 09:18:07 +0900

    2023/12/25 09:18:07

--- .config/zsh/styles.zsh
+++ .config/zsh/styles.zsh
@@ -1,13 +1,14 @@
 zstyle ':compinstall' filename "${HOME}/.zshrc"
+
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 zstyle ':completion:*' menu select=2
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' rehash 1
 zstyle ':completion:*' use-cache true
+zstyle ':completion:*:descriptions' format '[%d]'
 zstyle ':completion:*:default' menu yes select search
-# preview directory's content with lsd when completing cd
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'lsd --oneline --color="always" "${realpath}"'
-# switch group using `,` and `.`
+
 zstyle ':fzf-tab:*' switch-group ',' '.'
-zstyle ':fzf-tab:complete:*' fzf-pad 10
+zstyle ':fzf-tab:*' fzf-flags '--layout="default"'
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'lsd --oneline --color="always" "${realpath}"'
