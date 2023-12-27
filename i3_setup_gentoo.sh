23cd4a48f6f70248a2f688cdc70bdbe2e62d80aa
remon2207 <remon2207.develop@gmail.com>
2023-12-27 14:32:08 +0900

    2023/12/27 14:32:08

--- i3_setup_gentoo.sh
+++ i3_setup_gentoo.sh
@@ -76,10 +76,8 @@ setup() {
 services() {
   cp --archive "${current_dir}/.config/systemd/user/ssh-agent.service" "${HOME}/.config/systemd/user"
   cp --archive "${current_dir}/.config/systemd/user/"auto-backup.{service,timer} "${HOME}/.config/systemd/user"
-  sudo cp --archive "${current_dir}/etc/systemd/system/auto-lock@.service" /etc/systemd/system
 
   systemctl --user enable --now ssh-agent.service auto-backup.timer
-  sudo systemctl enable "auto-lock@${USER}.service"
 }
 
 main() {
