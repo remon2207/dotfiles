7e7fdda9964ec87ad9f874371c28e1a094874cd8
remon2207 <remon2207.develop@gmail.com>
2024-01-06 13:29:00 +0900

    2024/01/06 13:29:00

--- fstab_setup.sh
+++ fstab_setup.sh
@@ -4,10 +4,10 @@ set -eu
 
 nfs_opts='nfs rw,async,nodev,nosuid,noexec,noatime,nofail,noauto,nomand,noiversion,norelatime,lazytime,x-systemd.automount,x-systemd.idle-timeout=1min 0 0'
 nfs_fstab="# nfs
-192.168.1.33:/mnt/share01/rh     /mnt/share01/rh     ${nfs_opts}
-192.168.1.33:/mnt/share01/server /mnt/share01/server ${nfs_opts}"
+192.168.1.33:/mnt/share01/rh     /mnt/share/rh     ${nfs_opts}
+192.168.1.33:/mnt/share01/server /mnt/share/server ${nfs_opts}"
 
-sudo mkdir --parents /mnt/share01/{rh,server}
+sudo mkdir --parents /mnt/share/{rh,server}
 echo "${nfs_fstab}" | sudo tee --append /etc/fstab &> /dev/null
 
 sudo systemctl daemon-reload
