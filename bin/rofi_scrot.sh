ccc660862da7ca9fa2dea4f434faa61c83dd961e
remon2207 <remon2207.develop@gmail.com>
2024-01-17 01:26:18 +0900

    2024/01/17 01:26:18

--- bin/rofi_scrot.sh
+++ bin/rofi_scrot.sh
@@ -10,11 +10,11 @@ scrot_base() {
 }
 
 main() {
-  local -r window_text='Window'
-  local -r select_text='Select'
-  local -r all_text='All'
+  local -r window_text='ウィンドウ'
+  local -r select_text='選択'
+  local -r all_text='全て'
   local -r menus="${window_text}\n${select_text}\n${all_text}"
-  local -r selected="$(echo -e "${menus}" | /usr/bin/rofi -dmenu -p 'scrot')"
+  local -r selected="$(echo -e "${menus}" | /usr/bin/rofi -dmenu -l 3 -p 'scrot')"
 
   case "${selected}" in
     "${select_text}")
