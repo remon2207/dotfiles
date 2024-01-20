6a0b85eb4f93926203bb6cc42353f62523b0a23d
remon2207 <remon2207.develop@gmail.com>
2024-01-20 17:00:43 +0900

    2024/01/20 17:00:43

--- bin/backup.sh
+++ bin/backup.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-/usr/bin/rsync --archive --update "${HOME}/Pictures/" /mnt/share/rh/pictures
+/usr/bin/rsync --archive --update "${HOME}/Pictures/screenshots/" /mnt/share/rh/pictures/screenshots
 /usr/bin/rsync --archive --update "${HOME}/Videos/" /mnt/share/rh/videos
 /usr/bin/rsync --archive --update --delete --exclude='known*' "${HOME}/.ssh/" /mnt/share/rh/ssh
