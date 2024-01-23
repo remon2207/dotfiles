3afafa3cbe569e4b731f9e4a03d7e06710e9d4e9
remon2207 <remon2207.develop@gmail.com>
2024-01-23 15:03:01 +0900

    2024/01/23 15:03:01

--- bin/load_layout.sh
+++ bin/load_layout.sh
@@ -11,10 +11,12 @@ if [[ "${1}" == 'dvi' ]] && [[ "${2}" == 'dp2' ]] && [[ "${3}" == 'dp' ]]; then
   (/usr/bin/slack &)
   ("${HOME}/bin/JQuake.sh" &)
   (cd "${HOME}/src/KyoshinEewViewer" && "${HOME}/src/KyoshinEewViewer/KyoshinEewViewer" &)
-  # (/usr/bin/kitty --single-instance /usr/bin/btop &)
-  # (/usr/bin/kitty --single-instance /usr/bin/nvtop &)
-  (/usr/bin/alacritty --title='btop' --command='/usr/bin/btop' &)
-  (/usr/bin/alacritty --title='nvtop' --command='/usr/bin/nvtop' &)
+  (/usr/bin/kitty --single-instance /usr/bin/btop &)
+  (/usr/bin/kitty --single-instance /usr/bin/nvtop &)
+  # (/usr/bin/alacritty --title='btop' --command='/usr/bin/btop' &)
+  # (/usr/bin/alacritty --title='nvtop' --command='/usr/bin/nvtop' &)
+  # (/usr/bin/wezterm start btop &)
+  # (/usr/bin/wezterm start nvtop &)
   (/usr/bin/vivaldi-stable &)
 elif [[ "${1}" == 'dp2' ]]; then
   # ワークスペース3に移動し、レイアウトを展開する
@@ -30,8 +32,10 @@ elif [[ "${1}" == 'dvi' ]]; then
   /usr/bin/i3-msg "workspace --no-auto-back-and-forth 3; append_layout ${XDG_CONFIG_HOME}/i3/workspace_3.json"
 
   # 展開に必要なプログラムを起動する
-  # (/usr/bin/kitty --single-instance /usr/bin/btop &)
-  # (/usr/bin/kitty --single-instance /usr/bin/nvtop &)
-  (/usr/bin/alacritty --title='btop' --command='/usr/bin/btop' &)
-  (/usr/bin/alacritty --title='nvtop' --command='/usr/bin/nvtop' &)
+  (/usr/bin/kitty --single-instance /usr/bin/btop &)
+  (/usr/bin/kitty --single-instance /usr/bin/nvtop &)
+  # (/usr/bin/alacritty --title='btop' --command='/usr/bin/btop' &)
+  # (/usr/bin/alacritty --title='nvtop' --command='/usr/bin/nvtop' &)
+  # (/usr/bin/wezterm start btop &)
+  # (/usr/bin/wezterm start nvtop &)
 fi
