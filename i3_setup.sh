dde4ad57d9238133725bc1b891e75c763ad1d08f
remon2207 <39646984+remon2207@users.noreply.github.com>
2024-01-26 20:06:53 +0900

    i3_setup.sh の更新

--- i3_setup.sh
+++ i3_setup.sh
@@ -45,7 +45,7 @@ setup() {
   )
 
   rm --recursive --force "${HOME}/.xinitrc" "${HOME}/.config/"{i3,kitty}
-  sudo mkdir /etc/gtk-2.0
+  sudo mkdir --parents /etc/gtk-2.0 "${HOME}/.config/systemd/user"
 
   for home in "${home_symbolic[@]}"; do ln --symbolic --force --verbose "${CURRENT_DIR}/${home}" "${HOME}"; done
   for conf in "${conf_symbolic[@]}"; do ln --symbolic --force --verbose "${CURRENT_DIR}/.config/${conf}" "${HOME}/.config"; done
