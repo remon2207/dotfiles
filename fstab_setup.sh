c8e48843e0ade3e1438f38f8fafbdaf2b4683e48
remon2207 <remon2207.develop@gmail.com>
2024-02-12 14:23:40 +0900

    2024/02/12 14:23:40

--- fstab_setup.sh
+++ fstab_setup.sh
@@ -5,7 +5,7 @@ set -eu
 main() {
   local -r nfs_opts='nfs rw,async,nodev,nosuid,noexec,nouser,noatime,nofail,noauto,nomand,noiversion,norelatime,lazytime,_netdev,x-systemd.automount,x-systemd.idle-timeout=1min 0 0'
   local -r nfs_fstab="# nfs
-192.168.1.46:/       /mnt/nfs    ${nfs_opts}"
+192.168.1.46:/ /mnt/nfs ${nfs_opts}"
 
   sudo mkdir /mnt/nfs
   echo "${nfs_fstab}" | sudo tee --append /etc/fstab > /dev/null
